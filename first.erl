a3bbc2a74711ea1bcbf90d2d59d9677ad8c6898e
Gubin, Maksim <maksim.gubin@oxagile.com>
2016-02-16 09:28:31 +0300

    fix last step

--- first.erl
+++ first.erl
@@ -5,7 +5,8 @@ findPosition(StartList) ->
 MaxSUM=100000000000000000, % TODO %
 step(0,StartList,lists:sum(StartList),{MaxSUM,0}).
 
-step(_In_1,[_In|[]],_LastSum,Property)->Property;
+% Property == { abs ( MinSum ) , Element } %
+step(_In_1,[],_LastSum,Property)->Property;
 
 step(In_1,[In|Arr],LastSum,Property)-> 
 NewSum=LastSum-In_1-In,
